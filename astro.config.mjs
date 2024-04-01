a007f418ffc626c386800f2a4137266d057e902e
FerAleUtnSantaFe <fernandoale00@hotmail.com>
2024-04-01 16:36:57 -0300

    loginprueba1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
+import auth from "auth-astro";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
+  site: "https://v1-blog-d9knd6s94-fernando-ales-projects.vercel.app/",
   integrations: [tailwind(), icon({
     include: {
       lucide: ["*"]
     }
-  }), sitemap()]
+  }), sitemap(), auth()],
+  output: "server",
+  adapter: vercel()
 });
